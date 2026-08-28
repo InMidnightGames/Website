c1890d7a7606b3a953e3130fe66f06c9e14601fc
excellpepple <excellpepple@gmail.com>
2026-08-28 01:45:40 -0400

    testing workflows

--- web/src/pages/Home/components/HeroBanner.tsx
+++ web/src/pages/Home/components/HeroBanner.tsx
@@ -7,7 +7,7 @@ export default function HeroBanner() {
             className=" h-150 bg-cover bg-center bg-fixed flex flex-col items-center justify-center"
             style={{ backgroundImage: `url(${herobanner})` }}
         >
-            <p className="text-4xl text-center font-caslon font-bold text-[#EB4335]">Studio Logo</p>
+            <p className="text-6xl text-center font-caslon font-bold text-[#EB4335]">Studio Logo</p>
         </section>
     );
 }
