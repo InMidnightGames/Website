95526dbe80a838e57632e4e93a7f43856d8a22b4
excellpepple <excellpepple@gmail.com>
2026-08-28 03:08:19 -0400

    mobile friendly
    # Conflicts:
    #	web/src/pages/Home/components/HeroBanner.tsx

--- web/src/pages/Home/components/HeroBanner.tsx
+++ web/src/pages/Home/components/HeroBanner.tsx
@@ -5,7 +5,7 @@ export default function HeroBanner() {
     return (
         <section
             id="hero"
-            className="h-165 md:h-150 bg-cover scroll-mt-16 bg-center bg-fixed flex flex-col items-center justify-center"
+            className="h-165 md:h-150 md:bg-cover scroll-mt-16  bg-center bg-fixed flex flex-col items-center justify-center"
             style={{ backgroundImage: `url(${herobanner})` }}
         >
             <p className="text-6xl text-center font-caslon font-bold text-[#EB4335]">Studio Logo</p>
