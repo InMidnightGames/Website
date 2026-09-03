64e7991effdf0b9881919eb71b316f28a50d7cf9
excellpepple <excellpepple@gmail.com>
2026-09-02 22:55:56 -0400

    updating ProjectSection.tsx

--- web/src/pages/Home/components/ProjectSection.tsx
+++ web/src/pages/Home/components/ProjectSection.tsx
@@ -4,7 +4,7 @@ export default function ProjectSection() {
     const headerColor = "";
 
     return (
-        <section className=" font-caslon  lg:mb-20 md:mb-5 lg:mb-0  ">
+        <section className=" font-caslon  lg:mb-20 md:mb-5  lg:mb-0  ">
             {/* Fade overlay */}
             {/*<div className="absolute inset-0 bg-gradient-to-b from-[#10021e] via-transparent to-black" />*/}
             <div className="h-150 md:h-180 lg:h-150 portrait:h-200  w-screen flex flex-col md:portrait:grid md:portrait:grid-cols-2
@@ -17,7 +17,7 @@ export default function ProjectSection() {
                 </div>
 
                 <div className="flex flex-col  ">
-                    <h3 className={" mt-0 md:mt-15 xl:mt-5 lg:mt-20 font-bold  text-3xl md:portrait:text-4xl text-center md:portrait:text-start landscape:text-start lg:text-start" + headerColor}>OUR PROJECT</h3>
+                    <h3 className={" mt-0 md:mt-15 xl:mt-20 lg:mt-20 font-bold  text-3xl md:portrait:text-4xl text-center md:portrait:text-start landscape:text-start lg:text-start" + headerColor}>OUR PROJECT</h3>
                     <p className={"  max-w-85 mt-5 lg:landscape:text-justify lg:mt-5 text-lg landscape:text-md md:text-md  md:text-start landscape:text-start" + textColor}>
 
 
